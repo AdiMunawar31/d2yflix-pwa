9398e40fc1b505a342270c58e9dad30c2fe2ce6f
Adi Munawar <munawar.adi31@gmail.com>
2025-02-26 08:48:22 +0700

    fix error in add cache

--- public/cache-helper.js
+++ public/cache-helper.js
@@ -3,45 +3,53 @@ const CACHE_NAME = 'd2yflix-cache-v1';
 const CacheHelper = {
   async cachingAppShell(requests) {
     const cache = await this._openCache();
-    cache.addAll(requests);
+    const validRequests = requests.filter((request) => 
+      request.startsWith('http')
+    ); 
+    
+    await cache.addAll(validRequests);
   },
 
   async deleteOldCache() {
     const cacheNames = await caches.keys();
-    cacheNames
-      .filter((name) => name !== CACHE_NAME)
-      .map((filteredName) => caches.delete(filteredName));
+    await Promise.all(
+      cacheNames
+        .filter((name) => name !== CACHE_NAME)
+        .map((filteredName) => caches.delete(filteredName))
+    );
   },
 
   async revalidateCache(request) {
-    const response = await caches.match(request);
+    if (!request.url.startsWith('http')) return fetch(request); // Abaikan permintaan non-HTTP
 
+    const response = await caches.match(request);
     if (response) {
-      this._fetchRequest(request);
+      this._fetchAndUpdateCache(request);
       return response;
     }
-    return this._fetchRequest(request);
+    return this._fetchAndUpdateCache(request);
   },
 
   async _openCache() {
     return caches.open(CACHE_NAME);
   },
 
-  async _fetchRequest(request) {
-    const response = await fetch(request);
+  async _fetchAndUpdateCache(request) {
+    try {
+      const response = await fetch(request);
+      if (!response || response.status !== 200) return response;
 
-    if (!response || response.status !== 200) {
+      await this._addCache(request, response.clone());
       return response;
+    } catch (error) {
+      return caches.match(request) || Response.error();
     }
-
-    await this._addCache(request);
-    return response;
   },
 
-  async _addCache(request) {
+  async _addCache(request, response) {
+    if (!request.url.startsWith('http')) return;
+    
     const cache = await this._openCache();
-    cache.add(request);
-  },
+    await cache.put(request, response);
+  }
 };
-
-// export default CacheHelper;
